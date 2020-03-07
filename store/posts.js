99f6f3e13a9ef89a18556af83ddca373725a650e
Askari <blgntobi@gmail.com>
2020-03-07 21:47:32 +0100

    changed some contents

--- store/posts.js
+++ store/posts.js
@@ -1,18 +1,18 @@
 export const state = () => ({
     all: [
       {
-        id: '4endusers',
-        title: 'Things we do for end users',
+        id: 'AboutMe',
+        title: 'About Me',
         content: 'This is a site that i would work on soonest. Don\'t hold your breath.'
       },
       {
-        id: 'poetry',
-        title: 'Did You Say Poetry?',
-        content: 'Maybe. Just maybe, i\'ll post poems!'
+        id: 'resume',
+        title: 'Check Out My Resume',
+        content: ''
       },
       {
-        id: 'movie-ratings',
-        title: 'I really don\'t Cap',
+        id: 'profile',
+        title: 'Github Projects Links',
         content: 'I may soon start yarns on movies i hope you guys should see. till then!'
       }
     ]
